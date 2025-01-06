1c12b4abc3a70c0956d0b1fe8c9c39f75012edcc
2gooNo <143684446+2gooNo@users.noreply.github.com>
2025-01-06 14:51:20 +0800

    daily push

--- solutions.cpp
+++ solutions.cpp
@@ -1,18 +1,38 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
-#include <cmath>
-#include <map>
-#include <set>
 using namespace std;
-int main(){
-    ios_base::sync_with_stdio(0);
-cin.tie(NULL);
-cout.tie(NULL);
-int n,k,i;
-cin >>n;
-for(i=0;i<n;i++){
-    cin >>k;
-}
-cout <<1;
+
+int main()
+{
+    long long n, i, x, j,b,c;
+    cin >> n>>b;
+    vector <int> d;
+    for(i=0;i<b;i++){
+        cin >>c;
+        d.push_back(c);
     }
+    vector <int> a;
+    a.push_back(1);
+     a.push_back(2);
+        a.push_back(4);
+        bool e;
+    for (i = 3; i < n; i++)
+    {
+        e=0;
+        for(j=0;j<d.size();j++){
+            if(i==d[j]-1){
+                e=1;
+                d.erase(d.begin()+j);
+                break;
+            }
+        }
+        if(e){
+           a[i]=0;
+        }else{
+              a[i] = a[i - 2] + a[i - 1]+a[i-3];
+        }
+    }
+    cout <<a[n-1];
+    return 0;
+}
--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,1 +1,19 @@
-    haha.push_back({1,2});
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+    long long n, i, x, j;
+    cin >> n;
+    long long a[10000000];
+    a[0] = 1;
+    a[1]= 1;
+    for (i = 2; i <= n; i++)
+    {
+        a[i] = a[i - 2] + a[i - 1];
+    }
+    cout << a[n];
+    return 0;
+}
